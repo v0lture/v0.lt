026d3c0a796f30a03c8b19d68d15b6064d4a25fc
Amber <maxbycode@gmail.com>
2017-05-28 19:26:40 -0600

    more travis tinkering

--- tests.php
+++ tests.php
@@ -3,12 +3,21 @@
     $TEST = true;
     require "php/backend.php";
     require "php/URL.php";
+
+    class tests {
+
+        public function testA() {
+
+            echo "Creating a shortened link for https://v0lture.com...";
+            $dbc = mysqli_connect("localhost", "travis", "", "v0lt");
+
+            $backend = new Backend();
+            $URL = new URL($backend, $dbc);
+
+            var_dump($URL->create("https://v0lture.com"));
+
+        }
+
+    }
             
-    echo "Creating a shortened link for https://v0lture.com...";
-    $dbc = mysqli_connect("localhost", "root", "password", "v0lt");
-
-    $backend = new Backend();
-    $URL = new URL($backend, $dbc);
-
-    var_dump($URL->create("https://v0lture.com"));
 ?>
